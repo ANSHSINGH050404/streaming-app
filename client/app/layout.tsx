f7a5b774e0e6f293d129d8ccbe5c29b002ee3e7d
ANSHSINGH050404 <114014895+ANSHSINGH050404@users.noreply.github.com>
2026-04-16 13:57:03 +0530

    fix: resolve page export error, load global styles, and enable dark mode

---
+++ client/app/layout.tsx
@@ -0,0 +1,18 @@
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+import "./globals.css";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={cn("dark font-sans", geist.variable)}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
